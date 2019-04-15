31493391c3a7323aa5e3c12026e2a989f1e6aed0
teeuguima <mateeusguimaraes10@gmail.com>
2019-04-15 20:51:42 -0300

    OLHA ESSA MERDA AI

--- Client0001/src/client0001/Client0001.java
+++ Client0001/src/client0001/Client0001.java
@@ -14,10 +14,20 @@ import cliente1.model.Carro;
 import cliente1.model.Piloto;
 import cliente1.model.Equipe;
 import java.util.Iterator;
+import servidor0001.servidorFacade.ServidorFacade;
 
 public class Client0001 {
 
     String opc = "N";
+    ControladorCorrida cc;
+    ControladorDeDados cdd;
+    ServidorFacade sf;
+
+    private Client0001() {
+        this.cc = new ControladorCorrida();
+        this.cdd = new ControladorDeDados();
+        this.sf = new ServidorFacade();
+    }
 
     private int voltarMenu(String opc) {
         this.opc = opc;
@@ -31,86 +41,112 @@ public class Client0001 {
     public String getOpc() {
         return opc;
     }
+
+    public int menuPrincipal() throws IOException {
+        int opc;
+        System.out.println("Menu Internet dos Brinquedos");
+        System.out.println("1- Realizar Cadastro de Carros\n"
+                + "2- Realizar Cadastro de Participantes"
+                + "3- Iniciar Partida");
+        opc = Console.readInt();
+        return opc;
+    }
+
+    public int cadastroCarros() throws IOException {
+        String op;
+        System.out.println("Infome a tag do carro(ID)!");
+        String tag = Console.readString();
+
+        System.out.println("Informe a cor do carro!");
+        String cor = Console.readString();
+
+        System.out.println("Informe a equipe pertencente!");
+        String equipe = Console.readString();
+
+        Equipe e = this.sf.cadastrarEquipe(equipe);
+
+        Carro c = this.sf.cadastrarCarro(tag, cor, e);
+        
+        System.out.println(c.getCor()+ c.getTag()+ c.getEquipe().getNome());
+        System.out.println("Deseja cadastrar novamente? S/N");
+        op = Console.readString();
+        if (op.equals("S")) {
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
     
-    
+    public void iteraArrayCarros(){
+        System.out.println("OTÁRIO");
+        Iterator<Carro> iterCarro = cdd.itCarro();
+        boolean valor = iterCarro.hasNext();
+        if(valor == true){
+            System.out.println("Tem Próximo");
+        }else{
+            System.out.println("Vazio");
+        }
+    }
+
+    public int cadastroParticipantes() throws IOException {
+        /*  String op;
+        System.out.println("Informe o seu nome:");
+        String nome = Console.readString();
+
+        Piloto p = sf.cadastrarPiloto(nome, null);
+        System.out.println("Escolha o seu carro!");
+         */
+        
+        iteraArrayCarros();
+
+        /* String corCarro = Console.readString();
+
+        if (this.cdd.getCarroPorCor(corCarro) != null) {
+            System.out.println("Funfou!!!");
+        }
+
+        cc.adicionaParticipantes(p, cdd.getCarroPorCor(corCarro));
+        System.out.println("Deseja cadastrar novamente? S/N");
+        op = Console.readString();
+        if (op.equals("S")) {
+            return 1;
+        } else {
+            return 0;
+        }
+         */
+        return 0;
+    }
 
     public static void main(String[] args) throws IOException {
         Client0001 client = new Client0001();
-        ControladorCorrida cc = new ControladorCorrida();
-        ControladorDeDados cdd = new ControladorDeDados();
-        ControladorFactory ccf = new ControladorFactory() {
-        };
+        int repeat = 0;
 
-        int opc;
-        String op;
         do {
-
-            System.out.println("Menu Internet dos Brinquedos");
-            System.out.println("1- Realizar Cadastro de Carros\n"
-                    + "2- Realizar Cadastro de Participantes"
-                    + "3- Iniciar Partida");
-            opc = Console.readInt();
-
-            switch (opc) {
+            int controle = client.menuPrincipal();
+            switch (controle) {
 
                 case 1:
                     do {
-                        System.out.println("Infome a tag do carro(ID)!");
-                        String tag = Console.readString();
-
-                        System.out.println("Informe a cor do carro!");
-                        String cor = Console.readString();
-
-                        System.out.println("Informe a equipe pertencente!");
-                        String equipe = Console.readString();
-                        
-                        Equipe e = cdd.addEquipe(equipe);
-
-                        Carro c = cdd.addCarros(tag, cor, e);
-                        
-                        System.out.println("Deseja cadastrar novamente? S/N");
-                       op = Console.readString();
-                       client.voltarMenu(op);
-
-                    } while (client.getOpc().equals(op));
+                        repeat = client.cadastroCarros();
+                    } while (repeat == 1);
                     break;
+
                 case 2:
                     do {
-                        System.out.println("Informe o seu nome:");
-                        String nome = Console.readString();
-
-                        Piloto p = cdd.addPiloto(nome,null);
-                        System.out.println("Escolha o seu carro!");
-                        Iterator iter_Cars = cdd.itCarro();
-                        int count = 0;
-                        while (iter_Cars.hasNext()) {
-                            Carro c = (Carro) iter_Cars.next();
-                            System.out.println(count + 1 + " - " + c.getCor());
-                            count++;
-                        }
-                        String corCarro = Console.readString();
-
-                        if (cdd.getCarroPorCor(corCarro) != null) {
-                            System.out.println("Funfou!!!");
-                        }
-
-                        cc.adicionaParticipantes(p, cdd.getCarroPorCor(corCarro));
-                        System.out.println("Deseja cadastrar novamente? S/N");
-                        op = Console.readString();
-                        break;
-                    } while (client.voltarMenu(op) == 1);
+                        repeat = client.cadastroParticipantes();
+                    } while (repeat == 1);
+                    break;
 
             }
+        } while (repeat == 0);
 
-        } while (client.getOpc().equals("N"));
     }
-
     /*System.out.println("Realizar novo cadastro?\n" + "S/N");
             opcao = Console.readString();
             
             //} while (opcao.equals('S'));
-     */
- /*
+     */ /*
         System.out.println("Bem vindo cliente");
         Socket socket = new Socket("localhost", 5555);
         System.out.println("Cliente conectado");
@@ -125,4 +161,5 @@ public class Client0001 {
         System.out.println("return Message is=" + returnMessage.getNome());
         socket.close();
      */
+
 }
