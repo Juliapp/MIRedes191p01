c4770ad9677d18dc686e0ee29be687e9288bb5c1
juli <juli@juli-PC>
2019-04-26 17:47:20 -0300

    Javadoc da classe ControladorFactory

--- Servidor0001/src/servidor/controladores/ControladorFactory.java
+++ Servidor0001/src/servidor/controladores/ControladorFactory.java
@@ -6,55 +6,51 @@ import servidor.model.Equipe;
 import servidor.model.Jogador;
 import servidor.model.Piloto;
 
-/**
- *
- * @author juli
- */
 public class ControladorFactory {
 
     /**
-     *
+     *Factory é um projeto de sistema que "fabrica" objeto e devolve suas referências 
      */
     public ControladorFactory() {} 
     
     /**
-     *
-     * @param tag
-     * @param cor
-     * @param e
-     * @return
+     *Fabrica um objeto do tipo Carro
+     * @param tag Tag do carro
+     * @param cor cor do caro
+     * @param equipe equipe qual o carro recebe
+     * @return carro fabricado
      */
-    public Carro factoryC(String tag, String cor, Equipe e){
-        return new Carro(tag, cor, e);
+    public Carro factoryC(String tag, String cor, Equipe equipe){
+        return new Carro(tag, cor, equipe);
     }    
     
     /**
-     *
-     * @param nome
-     * @return
+     *Fabrica um objeto do tipo Equipe
+     * @param nome nome da Equipe
+     * @return equipe fabricada
      */
     public Equipe factoryE(String nome){
         return new Equipe(nome);
     }
     
     /**
-     *
-     * @param nome
-     * @param foto
-     * @return
+     *Fabrica um objeto do tipo Piloto
+     * @param nome nome do piloto
+     * @param foto foto do piloto
+     * @return piloto fabricado
      */
     public Piloto factoryP(String nome, String foto){
         return new Piloto(nome, foto);
     }
     
     /**
-     *
-     * @param c
-     * @param p
-     * @return
+     *Fabrica um objeto do tipo Jogador
+     * @param carro carro do joador
+     * @param piloto piloto
+     * @return jogador fabricado 
      */
-    public Jogador factoryJog(Carro c, Piloto p) {
-        return new Jogador(c, p);
+    public Jogador factoryJog(Carro carro, Piloto piloto) {
+        return new Jogador(carro, piloto);
     }
     
     
